95811b65321cada1b230b5973e8f16ad0df183e9
Zach Brucker <zach.brucker@gmail.com>
2021-02-18 17:56:31 -0600

    minor adjustments

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import AddCard from "./pages/AddCard";
 import Decks from "./pages/Decks";
 import Deck from "./pages/Deck";
 import Auth from "./pages/Auth";
+import Quiz from "./pages/Quiz";
 
 
 // Utilize react router to render component based on path
@@ -17,12 +18,14 @@ class App extends Component {
           <Route path='/home' component={Header}/>  
             <Route path='/decks' component={Header}/>  
             <Route path='/deck' component={Header}/>  
+            <Route path='/quiz' component={Header}/>  
           <Switch>
             <Route exact path='/' component={Auth}/>  
             <Route exact path='/home' component={AddCard}/>
             <Route exact path='/decks' component={Decks}/>
             <Route exact path='/deck' component={Deck}/>
             <Route exact path='/auth' component={Auth}/>
+            <Route exact path='/quiz' component={Quiz}/>
           </Switch>
           </div>
         </Router>
